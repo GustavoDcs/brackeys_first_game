c7afb167fecacda0d4d15c663b8e164f864608d9
Gustavo Silva <contato@gustavodcs.com>
2024-05-04 19:54:45 -0300

    feat: increment player animatino

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,25 +1,42 @@
 extends CharacterBody2D
 
-
 const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
+@onready var animated_sprite = $AnimatedSprite2D
 
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
+		animated_sprite.play("falling")
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("control_jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		print("jumping")
+		animated_sprite.play("jump")
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	#Get the input direction: -1, 0, 1
 	var direction = Input.get_axis("control_left", "control_right")
+
+	#Flip the sprite
+	if direction > 0:
+		animated_sprite.flip_h = false		
+	elif direction < 0:
+		animated_sprite.flip_h = true
+		
+	# Play animations
+	if is_on_floor():
+		if direction == 0:
+			animated_sprite.play("idle")
+		else:
+			animated_sprite.play("run")
+	
+
+	# Handle movement
 	if direction:
 		velocity.x = direction * SPEED
 	else:
