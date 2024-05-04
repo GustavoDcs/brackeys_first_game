b2001f3063e6d11b5321ff9fb6262a11d3633a30
Gustavo Silva <contato@gustavodcs.com>
2024-05-04 19:11:22 -0300

    feat: increment slime logic

--- scripts/slime.gd
+++ scripts/slime.gd
@@ -1,12 +1,13 @@
-extends CollisionShape2D
+extends Node2D
 
 const SPEED = 60
 
 var direction = 1
 
-@onready var ray_cast_right = $"../../RayCastRight"
-@onready var ray_cast_left = $"../../RayCastLeft"
-@onready var animated_sprite = $"../../AnimatedSprite2D"
+@onready var animated_sprite = $AnimatedSprite2D
+@onready var ray_cast_right = $RayCastRight
+@onready var ray_cast_left = $RayCastLeft
+
 
 func _process(delta):
 	if ray_cast_right.is_colliding():
