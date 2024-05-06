9eb2f8f95e366b08877eb3c71bb04d27af136974
Gustavo Silva <contato@gustavodcs.com>
2024-05-06 12:54:32 -0300

    feat: implement dash with running

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,17 +1,26 @@
 extends CharacterBody2D
 
-const MAX_WALK_SPEED = 100.0 
-const MAX_RUN_SPEED = 200.0  
-const WALK_ACCELERATION = 300.0 
-const RUN_ACCELERATION = 100.0 
-const DECELERATION = 1000.0  
+const MAX_WALK_SPEED = 100.0
+const WALK_ACCELERATION = 300.0
+
+const MAX_RUN_SPEED = 200.0
+const RUN_ACCELERATION = 100.0
+
+const DECELERATION = 1000.0
+
 const JUMP_VELOCITY = -300.0
 const FAST_FALL_SPEED = 1000.0
 
+const DASH_DURATION = 0.2  
+const DASH_SPEED = 200.0   
+
 var is_dead = false
-var is_dashing = false
+var is_running = false
+var is_dashing = false 
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+var dash_timer = 0.0
 
 @onready var animated_sprite = $AnimatedSprite2D
 
@@ -20,15 +29,29 @@ func _physics_process(delta):
 	var target_velocity_x = direction * MAX_WALK_SPEED
 
 	# Check if dash button is pressed and player is moving
-	is_dashing = Input.is_action_pressed("control_sprint") and direction != 0
-	if is_dashing:
+	is_running = Input.is_action_pressed("control_sprint") and direction != 0
+	if is_running:
 		target_velocity_x = direction * MAX_RUN_SPEED  # Apply higher speed limit for dashing
 
 	# Accelerate or decelerate towards the target velocity
 	if velocity.x < target_velocity_x:
-		velocity.x = min(velocity.x + (RUN_ACCELERATION if is_dashing else WALK_ACCELERATION) * delta, target_velocity_x)
+		velocity.x = min(velocity.x + (RUN_ACCELERATION if is_running else WALK_ACCELERATION) * delta, target_velocity_x)
 	elif velocity.x > target_velocity_x:
-		velocity.x = max(velocity.x - (RUN_ACCELERATION if is_dashing else WALK_ACCELERATION) * delta, target_velocity_x)
+		velocity.x = max(velocity.x - (RUN_ACCELERATION if is_running else WALK_ACCELERATION) * delta, target_velocity_x)
+
+	# Handle dashing
+	if Input.is_action_just_pressed("control_dash") and not is_dashing and velocity.x != 0:
+		print("Dashing")
+		is_dashing = true
+		dash_timer = DASH_DURATION
+		velocity.x += DASH_SPEED * direction
+
+	if is_dashing:
+		if dash_timer > 0:
+			dash_timer -= delta
+		else:
+			is_dashing = false
+			velocity.x = 0
 
 	# Add the gravity.
 	if not is_on_floor():
@@ -48,13 +71,13 @@ func _physics_process(delta):
 
 	# Play animations
 	if not is_dead:
-		if is_dashing:
-			animated_sprite.play("dash")
+		if is_running:
+			animated_sprite.play("run")
 		elif is_on_floor():
 			if direction == 0:
 				animated_sprite.play("idle")
 			else:
-				animated_sprite.play("run")
+				animated_sprite.play("walk")
 		elif velocity.y > 0:
 			animated_sprite.play("falling")
 		elif velocity.y < 0:
